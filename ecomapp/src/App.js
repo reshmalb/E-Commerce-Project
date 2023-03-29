59cea4bf6cf3dd2a7f8cb4f599841d2c468adc7b
reshmalb <reshma.lb27@gmail.com>
2023-03-30 00:36:54 +0530

    header and footer added

--- ecomapp/src/App.js
+++ ecomapp/src/App.js
@@ -1,25 +1,12 @@
 import React,{useState} from 'react';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
-import { Store } from './pages/Store';
-import DisplayItems from './Components/DisplayItems';
-import Header from './Components/Layout/Header';
+import Store from './pages/Store'
 import Cart from './Components/CartButton/Cart';
 import ShoppingCartProvider from './store/ShoppingCartProvider';
 import {Routes,Route, BrowserRouter} from "react-router-dom"
-import { Container } from 'react-bootstrap';
-import {createBrowserRouter,RouterProvider, createRoutesFromElements} from 'react-router-dom'
-
-// const routeDefinitions=createRoutesFromElements(
-//  <Route>
-//   <Route path="/" element={<Home/>}/>
-//    <Route path="/about" element={<About/>}/>
-//   <Route path="/store" element={<Store/>}/> 
-// </Route> );
-
-// const router=createBrowserRouter(routeDefinitions);
-
-
+import NavBar from './Components/Layout/Header';
+import Footer from './Components/Layout/Footer';
 const  App=()=> {
   const [isCartShown,setCartShown]=useState(false);
   const hideCartHandler=()=>{
@@ -31,13 +18,17 @@ const  App=()=> {
   return (
 <BrowserRouter>
 <ShoppingCartProvider>
-    {isCartShown&& <Cart isShown={isCartShown} onClose={hideCartHandler}/>} 
-      <Header onShowCart={showCartHandler} />
-     {/* <RouterProvider router={router}></RouterProvider> */}
+<NavBar onShowCart={showCartHandler} />
+<main>
+  {isCartShown&& <Cart isShown={isCartShown} onClose={hideCartHandler}/>} 
+
+</main>
+    
+     <Footer/>
  <Routes>
   <Route path="/" element={<Home/>}/>
    <Route path="/about" element={<About/>}/>
-  <Route path="/store" element={<DisplayItems/>}/> 
+  <Route path="/store" element={<Store/>}/> 
 </Routes>
                   
     </ShoppingCartProvider>
--- ecomapp/src/pages/Store.js
+++ ecomapp/src/pages/Store.js
@@ -1,3 +1,62 @@
-export const Store=()=>{
-    return <h1>Store</h1>
+import {Card,Button,Col,Row} from 'react-bootstrap';
+import {ProductData} from '../Data/ProductData'
+import React,{useContext, useState} from 'react';
+import ShoppingCartContext from '../store/ShoppingContext';
+
+
+const Store=()=> {
+    const [productItems,setProductItems]=useState(ProductData)
+    const ctx=useContext(ShoppingCartContext);
+
+    const addItemToCartHandler=(item)=>{
+       console.log(item);
+         ctx.increaseCartItem({
+               id:item.id,
+               title:item.title,
+               price:item.price,
+               imageUrl:item.imageUrl,
+               quantity:1
+               });
+    }
+
+  return (
+    <>
+    <h1> Music</h1>
+      <Row md={2} xs={1} lg={2} 
+        className="g-5 ms-5 me-5">
+          {productItems.map((item)=>{
+               return(   <Col key={item.id}>
+                      <Card className='b-10'>
+                          <Card.Img src={item.imageUrl}
+                            height="200px"
+                            width="100px"
+                            style={{objectFit:"cover"}}>
+                          </Card.Img>
+                          <Card.Body className='d-flex flex-column'>
+                             <Card.Title
+                             className='d-flex justify-content-between
+                             align-items-baseline mb-4'>
+                                <span className='fs-2'>{item.title}</span>
+                                <span className='ms-2 text-muted'>${item.price}</span>
+                             </Card.Title>
+                             <div className="mt-auto">
+                              <Button className='w-100' type="submit" onClick={addItemToCartHandler.bind(null,item)}>
+                                +ADD TO CART
+                              </Button>
+
+                             </div>
+
+                          </Card.Body>
+
+                      </Card>
+                  </Col>)
+          })}
+        
+
+      </Row>
+    </>
+   
+  );
 }
+
+export default Store;
