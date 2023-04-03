5784761fed58c4b09d9496ab3de67b7ae3af0f9c
reshmalb <reshma.lb27@gmail.com>
2023-04-03 23:17:52 +0530

    login

--- ecomapp/src/App.js
+++ ecomapp/src/App.js
@@ -1,37 +1,38 @@
-import React,{useState} from 'react';
+import React,{useContext, useState} from 'react';
 import  Home from './pages/Home';
 import About from './pages/About';
 import Store from './pages/Store'
 import ShoppingCartProvider from './store/ShoppingCartProvider';
-import {Route} from "react-router-dom";
+import {Redirect, Route} from "react-router-dom";
 import Header from './Components/Layout/Header';
 import Footer from './Components/Layout/Footer';
 import ContactUs from './pages/ContactUs';
 import ProductDetails from './pages/ProductDetails';
 import LoginPage from './pages/LoginPage';
-
+import AuthContext from './store/AuthContext';
 
 const  App=()=> {
-
+  const athctx=useContext(AuthContext);
+  console.log("isloggedin",athctx.isLoggedin)
   
-// {/* <BrowserRouter>
-// <ShoppingCartProvider>
-// <NavBar onShowCart={showCartHandler} />
-// {/* <main> */}
-//   {isCartShown&& <Cart isShown={isCartShown} onClose={hideCartHandler}/>} 
 
-// </main>
     
 return (
 
   <ShoppingCartProvider>
   <Header/>
   <Route path="/">  <Home/>  </Route>
-  <Route path="/store" exact> <Store/></Route>
+  {athctx.isLoggedin && <Route path='/store'><Store/></Route>}  
+  {!athctx.isLoggedin &&  <Route path="/store" >
+     <Redirect to='/login'></Redirect></Route> }
+  
+  
+ 
   <Route path="/store/:productid" ><ProductDetails/></Route>
   <Route path="/about"><About/></Route>
   <Route path="/contactus" ><ContactUs/></Route>
-  <Route path="/login"> <LoginPage/></Route>
+  {!athctx.isLoggedin && <Route path='/login'> <LoginPage/></Route>}
+ 
   <Route path="/logout"> <Home/></Route>
 
   
